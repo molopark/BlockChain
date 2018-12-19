3416cb857e6d42b83f9827e1a5fce54183327c65
molopark <molopark@gmail.com>
2018-12-19 17:05:58 +0900

    javascript기반 블록체인개발강좌

--- vote.sol
+++ vote.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.1;
+pragma solidity 0.4.24;
 
 contract vote {
 
@@ -15,16 +15,31 @@ contract vote {
     mapping(address => bool) voted;
 
     // event
+    bool live;
+    address owner;
     event AddCandidator(string name);
     event UpVote(string candidator, uint upVote);
+    event FinishVote(bool live);
+    event Voting(address owner);
 
     // modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
 
     // constructor
+    constructor() public {
+        owner = msg.sender;
+        live = true;
+        // emit event
+        emit Voting(owner);
+    }
 
     // candidator
-    function addCandidator(string _name) public {
+    function addCandidator(string _name) public onlyOwner{
         require(candidatorList.length < 5);
+        require(live == true);
         candidatorList.push(candidator(_name, 0));
         // emit event
         emit AddCandidator(_name);
@@ -32,6 +47,7 @@ contract vote {
 
     // voting
     function upVote(uint _indexOfCandidator) public {
+        require(live == true);
         require(_indexOfCandidator < candidatorList.length);
         require(voted[msg.sender] == false);
         candidatorList[_indexOfCandidator].upVote++;
@@ -41,7 +57,10 @@ contract vote {
     }
 
     // finish vote
-    function finishVote() public {
-
+    function finishVote() public onlyOwner {
+        require(live == true);
+        live = false;
+        //emit event
+        emit FinishVote(live);
     }
 }
